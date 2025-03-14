9dec0adafa9d8bfd53e218f7f5a27a4d5e042886
Izadora Montenegro <izadoraoamontenegro@gmail.com>
2025-03-14 20:24:33 -0300

    commit da developer

--- MeuPrimeiroRepositorio/ContentView.swift
+++ MeuPrimeiroRepositorio/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Meu primeiro commit")
+            Text("DEVELOPER")
         }
         .padding()
     }
